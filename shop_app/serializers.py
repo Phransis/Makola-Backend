99fc5e03feddb707d454085051933e5297076e6a
Handla <duofrancis11@gmail.com>
2025-05-01 22:01:31 +0000

    product_detail endpoint created

--- shop_app/serializers.py
+++ shop_app/serializers.py
@@ -6,3 +6,16 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = '__all__'
         # read_only_fields = ['slug', 'created_at']
+
+class DetailedProductSerializer(serializers.ModelSerializer):
+    similar_products = serializers.SerializerMethodField()
+    class Meta:
+        model = Product
+        # fields = '__all__'
+        # read_only_fields = ['id', 'name', 'price','slug', 'image', 'description', 'created_at', 'updated_at', 'similar_products']
+        fields = ['id', 'name', 'price','slug', 'image', 'description', 'created_at', 'updated_at', 'similar_products']
+
+    def get_similar_products(self, product):
+        # Assuming you have a method to get similar products
+        products = Product.objects.filter(category=product.category).exclude(id=product.id)[:5]
+        return ProductSerializer(products, many=True).data
--- shop_app/views.py
+++ shop_app/views.py
@@ -19,3 +19,25 @@ def products_list(request):
     products = Product.objects.all()
     serializer = ProductSerializer(products, many=True)
     return Response(serializer.data)
+
+@api_view(['GET'])
+def product_detail(request, slug):
+    try:
+        product = Product.objects.get(slug=slug)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data)
+    except Product.DoesNotExist:
+        return Response({"error": "Product not found"}, status=404)
+
+@api_view(['GET'])
+def product_category(request, category_slug):
+    products = Product.objects.filter(category__slug=category_slug)
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def product_search(request):
+    query = request.GET.get('q', '')
+    products = Product.objects.filter(name__icontains=query)
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
